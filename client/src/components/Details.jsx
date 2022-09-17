3358a8b6a625730dff2c9ce069710a536381689f
Andrew Wang <andrewwang@v1040-wn-rt-a-101-82.campus-dynamic.uwaterloo.ca>
2022-09-17 18:06:19 -0400

    Remove submodule entry

---
+++ client/src/components/Details.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Card } from "primereact/card";
+// import Layout from "./components/Layout";
+
+function Details() {
+  return <Card title="Description">Content</Card>;
+}
+
+export default Details;
